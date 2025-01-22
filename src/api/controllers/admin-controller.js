d67ab82b5329f0a57bda8db2bf372f5942f9e108
Mohamed Elshaarawy <elsharawy234@gmail.com>
2025-01-22 02:30:59 +0200

    typooooo

--- src/api/controllers/admin-controller.js
+++ src/api/controllers/admin-controller.js
@@ -2,4 +2,4 @@ export { default as createAdmission } from './admin/create-admission.js'
 export { default as getHospitalAdmissions } from './admin/get-hospital-admissions.js'
 export { default as getHospitalDoctors } from './admin/get-hospital-doctors.js'
 export { register } from './register.js'
-export { login }
+export { login } from './login.js'
