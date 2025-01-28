a25140128310e421c0fc9552daa8b228f9ed4580
Mohamed Elshaarawy <elsharawy234@gmail.com>
2025-01-28 22:05:49 +0200

    add doctor name to getHospitalAdmissions

--- src/api/controllers/admin/get-hospital-admissions.js
+++ src/api/controllers/admin/get-hospital-admissions.js
@@ -1,9 +1,14 @@
-import { Admin, Admission } from "../../../models/models-index.js";
+import { Admin, Admission, Doctor } from "../../../models/models-index.js";
+import { UnauthorizedError } from "../../../utils/error-handler.js";
 import { sendError } from "../../../utils/response-handler.js";
 
 export default async (req, res) => {
     try {
         const adminId = req.auth.payload.sub;
+
+        if (!adminId) {
+            throw new UnauthorizedError('Admin ID not found in token');
+        }
         const admin = await Admin.findByPk(adminId, {
             attributes: ['hospital_id']
         });
@@ -15,7 +20,17 @@ export default async (req, res) => {
             },
         });
 
-        res.status(200).send(admissions);
+        const admissionsWithDoctorNames = await Promise.all(admissions.map(async (admission) => {
+            const doctor = await Doctor.findByPk(admission.doctor_id, {
+                attributes: ['full_name']
+            });
+            return {
+                ...admission.toJSON(),
+                doctor_name: doctor.full_name
+            };
+        }));
+
+        res.status(200).send(admissionsWithDoctorNames);
     } catch (error) {
         return sendError(res, error);
     }
