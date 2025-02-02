a285c9da493cd84a03f1f743ad3ed906bcae9d97
Youssef Elharty <youssefashraf246@gmail.com>
2025-02-02 22:57:40 +0200

    Added log

--- src/loaders/mongoose-loader.js
+++ src/loaders/mongoose-loader.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import { mongoUri } from '../config/config.js';
 
 export default async () => {
+  console.log(`MongoDB Connecting at ${mongoUri}` );
   mongoose.connect(mongoUri, {})
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
